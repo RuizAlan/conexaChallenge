455eda48e8d4a283bdfde1e3e15c1d47332cbaf1
Alan Ruiz <fruizalan@gmail.com>
2021-05-22 15:22:45 -0300

    creacion de rutas y controllers de login

---
+++ back/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    register,
+    login,
+    forgotPassword,
+    resetPassword,
+} = require('../controllers/auth');
+
+router.post('/register', register);
+router.post('/login', login);
+router.post('/forgotPassword', forgotPassword);
+router.put('/resetPassword/:resetToken', resetPassword);
+
+module.exports = router;
--- back/server.js
+++ back/server.js
@@ -1,6 +1,11 @@
+require('dotenv').config({ path: './config.env' });
 const express = require('express');
-
+const auth = require('./routes/auth');
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 
