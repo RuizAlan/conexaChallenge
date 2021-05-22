cdab443814a3ad97808ad1696328c9a6d29dc5d5
Alan Ruiz <fruizalan@gmail.com>
2021-05-22 18:42:21 -0300

    finalizacion del backend, se agrego sendmailer, private controller, middlewares, models, private route y utils

--- back/server.js
+++ back/server.js
@@ -1,12 +1,30 @@
 require('dotenv').config({ path: './config.env' });
 const express = require('express');
+const connectDB = require('./config/db');
 const auth = require('./routes/auth');
+const private = require('./routes/private');
+const errorHandler = require('./middleware/error');
+
+// Connect DB
+connectDB();
+
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/auth', auth);
+app.use('/api/private', private);
+
+// Error Handler (Should be last piece of middleware)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+    console.log(`Server running on port ${PORT}`)
+);
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Logged Error: ${err}`);
+    server.close(() => process.exit(1));
+});
